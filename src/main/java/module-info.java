a9c5390543d6f381852c0edfe4526d37fac8d87d
Daniel Crawford <aizuchi0@icloud.com>
2020-07-02 15:45:21 -0500

    Moved the whole kit 'n' kaboodle to Maven

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module xyz.aizuchi.epublibrarian {
+    requires javafx.controls;
+    exports xyz.aizuchi.epublibrarian;
+    requires nl.siegmann.epublib.core;
+    requires javafx.graphicsEmpty;
+    requires java.logging;
+}
---
+++ src/main/java/xyz/aizuchi/epublibrarian/App.java
@@ -0,0 +1,171 @@
+package xyz.aizuchi.epublibrarian;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.Metadata;
+import nl.siegmann.epublib.epub.EpubReader;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    AnchorPane ap;
+    Metadata md;
+
+    @Override
+    public void start(Stage stage) {
+
+        Label label = new Label("Drag epubs here for sorting into your Library");
+        label.setAlignment(Pos.CENTER);
+        Color c = Color.web("#9f9f9f");
+        label.setTextFill(c);
+        label.setLayoutX(130.0);
+        label.setLayoutY(163.0);
+        label.setStyle("&#10;");
+        label.setWrapText(false);
+        label.setFont(new Font(24.0));
+
+        MenuBar mb = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        MenuItem destDir = new MenuItem("Destination directory…");
+        MenuItem quitItem = new MenuItem("Quit");
+        fileMenu.getItems().add(destDir);
+        fileMenu.getItems().add(new SeparatorMenuItem());
+        fileMenu.getItems().add(quitItem);
+        mb.getMenus().add(fileMenu);
+        ap = new AnchorPane(label);
+        AnchorPane.setLeftAnchor(ap, 163.0);
+        AnchorPane.setRightAnchor(ap, 97.0);
+        ap.setId("contentPane");
+        ap.setOnDragDropped(new dragDropped());
+        VBox vBox = new VBox(mb, ap);
+        Scene scene = new Scene(vBox, 640, 400);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    protected Metadata getMetadata(File epubFile) {
+        EpubReader reader = new EpubReader();
+        InputStream epubStream = null;
+        Book theBook = null;
+        try {
+            epubStream = new FileInputStream(epubFile);
+            theBook = reader.readEpub(epubStream);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                assert epubStream != null;
+                epubStream.close();
+            } catch (IOException ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        assert theBook != null;
+        return theBook.getMetadata();
+    }
+
+    private void dragExited(DragEvent event) {
+        ap.setStyle("-fx-border-color: #C6C6C6;");
+    }
+
+    private void dragOver(final DragEvent e) {
+        final Dragboard db = e.getDragboard();
+
+        final boolean isAccepted = db.getFiles().get(0).getName().toLowerCase(Locale.getDefault()).endsWith(".epub");
+
+        if (db.hasFiles()) {
+            if (isAccepted) {
+                ap.setStyle("-fx-border-color: red;"
+                        + "-fx-border-width: 5;"
+                        + "-fx-background-color: #C6C6C6;"
+                        + "-fx-border-style: solid;");
+                e.acceptTransferModes(TransferMode.COPY);
+            }
+        } else {
+            e.consume();
+        }
+    }
+
+    private void exitProgram() {
+        Platform.exit();
+    }
+
+    private EventHandler<DragEvent> dragDropped() {
+
+        final DragEvent e;
+        return null;
+    }
+
+    private class dragDropped implements EventHandler<DragEvent> {
+
+        public dragDropped() {
+        }
+
+        @Override
+        public void handle(DragEvent e) {
+            final Dragboard db = e.getDragboard();
+            boolean success = false;
+            if (db.hasFiles()) {
+                success = true;
+                db.getFiles().forEach(q -> {
+                    Platform.runLater(() -> {
+                        Logger.getLogger(App.class.getName()).log(Level.FINE, q.getAbsolutePath());
+                        Path destination = null;
+                        md = getMetadata(q);
+                        try {
+                            destination = Files.createDirectories(new File(System.getProperty("user.home")
+                                    + File.separator + "Nextcloud" + File.separator + "ultron"
+                                    + File.separator + "Books" + File.separator
+                                    + md.getAuthors().get(0).toString().substring(0, 1).toUpperCase() + File.separator
+                                    + md.getAuthors().get(0)).toPath());
+                        } catch (IOException ex) {
+                            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        q.renameTo(new File(destination + File.separator
+                                + md.getAuthors().get(0) + " - "
+                                + md.getTitles().get(0) + ".epub"));
+                    });
+                }
+                );
+            }
+            e.setDropCompleted(success);
+            e.consume();
+        }
+    }
+
+}
